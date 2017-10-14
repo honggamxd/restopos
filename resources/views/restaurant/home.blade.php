46f115cb158df7bc58c7ac2f7fd8a1207a0af54b
poy03 <jpgulayan@gmail.com>
2017-10-14 14:06:19 +0800

    changed label

--- resources/views/restaurant/home.blade.php
+++ resources/views/restaurant/home.blade.php
@@ -435,7 +435,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Order</h4>
+        <h4 class="modal-title">Cancellation of Orders</h4>
       </div>
       <div class="modal-body">
         <table class="ui unstackable sortable compact table">
