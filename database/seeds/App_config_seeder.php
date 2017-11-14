667940ed12236fd21ad43153c5c62de0611c0be8
poy03 <jpgulayan@gmail.com>
2017-11-14 09:55:22 +0800

    Modified Settlement Arrangements

--- database/seeds/App_config_seeder.php
+++ database/seeds/App_config_seeder.php
@@ -14,7 +14,7 @@ class App_config_seeder extends Seeder
       DB::table('app_config')->where('id',1)->update(
         ['settlements' => 'cash,credit,debit,cheque,guest_ledger,send_bill,free_of_charge,manager_meals,sales_office,representation',
         'badorder_settlements' => 'cancelled,bad_order,staff_charge',
-        'settlements_arrangements' => 'cash,credit,debit,cheque,guest_ledger,send_bill,free_of_charge,manager_meals,sales_office,representation,cancelled'
+        'settlements_arrangements' => 'cash,credit,debit,cheque,sales_office,guest_ledger,send_bill,free_of_charge,manager_meals,representation,cancelled'
         ]
       );
     }
