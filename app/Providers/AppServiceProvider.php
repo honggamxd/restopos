83785e6dfb72b353a80c9cea2ab59ec59f60c61e
poy03 <jpgulayan@gmail.com>
2018-01-25 20:41:34 +0800

    migration fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         Validator::extend('custom_min', function($attribute, $value, $parameters, $validator) {
             return $value>=$parameters[0];
         });
